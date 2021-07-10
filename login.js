c07159c49aa2b4dc8804dac535e1adca520ca7b9
sisbhumi <87226560+sisbhumi@users.noreply.github.com>
2021-07-10 15:00:14 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+function fun1()
+{
+	var name = document.getElementById("text1").value ;
+	var paswrd = document.getElementById("pass").value;
+	
+	if( paswrd == '12345' && name == "Bhumika" )
+		alert('Login Successfull') ;
+} 
